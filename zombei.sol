a2802089e99039f75ad6b9af4bdee728b4722f08
Sadi1377 <saeedsadi93@gmail.com>
2020-12-09 16:44:08 +0330

    khate akhar avaz shode

--- zombei.sol
+++ zombei.sol
@@ -18,7 +18,7 @@ contract ZombieFactory {
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
+        return rand % dnaModulu;
     }
 
     // start here
